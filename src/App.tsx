d17a0a8bc243f0c4e0bcee998755c1a887ff378f
Eva Niehaus <eniehaus111@gmail.com>
2023-07-25 00:11:11 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+import './App.css';
+import {Todolist} from './Todolist';
+
+export type filterType = 'all' | 'active' | 'completed'
+
+function App() {
+
+    // const tasks = [
+    //     {id: 1, title: "HTML&CSS", isDone: true},
+    //     {id: 2, title: "JS", isDone: true},
+    //     {id: 3, title: "ReactJS", isDone: false}
+    // ]
+
+    const [tasks, setTasks] = useState([
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false}
+    ])
+
+    const removeTask = (taskId: number) => {
+        setTasks(tasks.filter(el => el.id !== taskId))
+    }
+
+    //filteredTasks ===filterKey
+
+
+
+    // const [filteredTasks, setFilteredTasks] = useState<filterType>('all')
+    // const tasksFilter = (filterKey: filterType) => {
+    //     setFilteredTasks(filterKey)
+    // }
+    //
+    // let durchschlag = tasks
+    // if (filteredTasks === 'active') {
+    //     durchschlag = tasks.filter(el => !el.isDone)
+    // }
+    // if (filteredTasks === 'completed') {
+    //     durchschlag = tasks.filter(el => el.isDone)
+    // }
+
+
+
+
+    return (
+        <div className = "App">
+            <Todolist title = "What to learn"
+                      tasks = {tasks}
+                      removeTask = {removeTask}
+
+            />
+
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Todolist.tsx
@@ -0,0 +1,70 @@
+import React, {useState} from 'react';
+import {filterType} from "./App";
+
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
+    removeTask: (taskId: number) => void
+
+}
+
+export function Todolist(props: PropsType) {
+
+    const [filteredTasks, setFilteredTasks] = useState<filterType>('all')
+
+    const tasksFilter = (filterKey: filterType) => {
+        setFilteredTasks(filterKey)
+    }
+
+    const filterFoo = () => {
+        switch (filteredTasks) {
+            case 'active':
+                return props.tasks.filter(el => !el.isDone)
+            case 'completed':
+                return props.tasks.filter(el => el.isDone)
+            default:
+                return props.tasks
+
+
+        }
+        // if (filteredTasks === 'active') {
+        //     durchschlag= props.tasks.filter(el => !el.isDone)
+        // }
+        // if (filteredTasks === 'completed') {
+        //     durchschlag= props.tasks.filter(el => el.isDone)
+        // }
+        // return durchschlag
+    }
+
+
+    return <div>
+        <h3>{props.title}</h3>
+        <div>
+            <input />
+            <button>+</button>
+        </div>
+        <ul>
+            {filterFoo().map(el => {
+                return (
+
+                    <li>
+                        <button onClick = {() => props.removeTask(el.id)}>X</button>
+                        <input type = "checkbox" checked = {el.isDone} />
+                        <span>{el.title}</span></li>
+                )
+            })}
+
+        </ul>
+        <div>
+            <button onClick = {() => tasksFilter('all')}>All</button>
+            <button onClick = {() => tasksFilter('active')}>Active</button>
+            <button onClick = {() => tasksFilter('completed')}>Completed</button>
+        </div>
+    </div>
+}
